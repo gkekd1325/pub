9ca67c293b347804edaf4f23e4633b772cb0933e
jimin <gkekd1325@naver.com>
2022-03-22 13:47:02 +0900

    02-2

---
+++ 02-2/js/script.js
@@ -0,0 +1,20 @@
+$(".menu").hover(
+    function(){
+        $(".sub").stop().slideDown();
+        $(".nav_back").stop().slideDown();
+    },
+    function(){
+        $(".sub").stop().slideUp();
+        $(".nav_back").stop().slideUp();
+    }
+);
+$(".notice li:eq(0)").click(
+    function(){
+        $("#popup").fadeIn();
+    }
+);
+$(".close").click(
+    function(){
+        $("#popup").fadeOut();
+    }
+);
